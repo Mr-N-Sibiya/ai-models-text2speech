d9fe30d4a18b91cb51f3565d7a3c22073e8d5a7c
Nhlahla Sibiya <78474460+Mr-N-Sibiya@users.noreply.github.com>
2024-03-09 23:06:19 +0000

    third commit, text part working well

--- Hello.py
+++ Hello.py
@@ -14,38 +14,90 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from moviepy.editor import concatenate_audioclips, AudioFileClip
+from replicate.client import Client
+from replicate import Client
+import streamlit_authenticator as stauch
+import tempfile
+import replicate
+import requests
 
 LOGGER = get_logger(__name__)
 
+chunks=""
+
+def text_upload(a=chunks):
+    #global replicate  # Add this line to indicate that you're using the global variable
+    print("runa wena")
+    # Authenticate replicate client
+    replicate = replicate.Client(api_token='r8_FPd0TDjuTIaLK6pJ86sWou8rWBBD4oC1dzWt0')
+    api_input_text = '\n'.join(chunks)
+
+    output = replicate.run(
+        "adirik/styletts2:989cb5ea6d2401314eb30685740cb9f6fd1c9001b8940659b406f952837ab5ac",
+        input={
+            "beta": 0.7,
+            "seed": 0,
+            "text": api_input_text,
+            "alpha": 0.3,
+            "diffusion_steps": 10,
+            "embedding_scale": 1.5
+        }
+    )
+    print(output)
+
+
+def pdf_upload():
+    print("write the code here")
 
 def run():
+
+    
+
     st.set_page_config(
-        page_title="Hello",
+        page_title="2 SpeechTTS",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+    
+    st.write("# Welcome to TTSModel 👋")
+    genre = st.radio(
+        "**How do you want to use 2_SpeechTTS**",
+        ["Write Text", "Upload PDF",],
+        captions = ["Enter Text to convert to speech", "Upload PDF file to convert to speech"],
+        key="qe12"
     )
+
+    if genre == 'Write Text':
+        input_text=[]
+        input_text = st.text_area("Please Enter Text to Convert to Speech", max_chars=100, key="qw13")
+        if st.button("Convert to Speech"):
+            # Call function to convert input_text to speech
+            pass
+            text_upload(a=input_text)
+
+
+
+
+    else: 
+        file_up = st.file_uploader("Choose PDF File", type="pdf", key="qr")
+        if st.button("Convert to Speech"):
+            if file_up is not None:
+                # Call function to convert file_up to speech
+                pass
+                pdf_upload()
+            else:
+                st.warning("Please upload a PDF file.")
+    print(input_text)
+
+    
+
+
 
 
 if __name__ == "__main__":
     run()
+
+
+
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This is the app you get when you run `streamlit hello`, extracted as its own app
 
 Edit [Hello.py](./Hello.py) to customize this app to your heart's desire. ❤️
 
-Check it out on [Streamlit Community Cloud](https://st-hello-app.streamlit.app/)
+Check it out on [Streamlit Community Cloud](    from replicate.client import Client
+    import replicate)
