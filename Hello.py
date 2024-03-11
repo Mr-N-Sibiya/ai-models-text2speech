a5c858ad5c311b49ce56b4e9bae599ba86367924
Nhlahla Sibiya <78474460+Mr-N-Sibiya@users.noreply.github.com>
2024-03-11 09:31:28 +0000

    third commit have code on, converting text to pdf

--- Hello.py
+++ Hello.py
@@ -19,39 +19,22 @@ from replicate.client import Client
 from replicate import Client
 import streamlit_authenticator as stauch
 import tempfile
-import replicate
+
 import requests
 
 LOGGER = get_logger(__name__)
 
-chunks=""
-
-def text_upload(a=chunks):
-    #global replicate  # Add this line to indicate that you're using the global variable
-    print("runa wena")
-    # Authenticate replicate client
-    replicate = replicate.Client(api_token='r8_FPd0TDjuTIaLK6pJ86sWou8rWBBD4oC1dzWt0')
-    api_input_text = '\n'.join(chunks)
-
-    output = replicate.run(
-        "adirik/styletts2:989cb5ea6d2401314eb30685740cb9f6fd1c9001b8940659b406f952837ab5ac",
-        input={
-            "beta": 0.7,
-            "seed": 0,
-            "text": api_input_text,
-            "alpha": 0.3,
-            "diffusion_steps": 10,
-            "embedding_scale": 1.5
-        }
-    )
-    print(output)
 
 
-def pdf_upload():
-    print("write the code here")
+
+
+
+
+
+
 
 def run():
-
+    import replicate
     
 
     st.set_page_config(
@@ -71,11 +54,27 @@ def run():
 
     if genre == 'Write Text':
         input_text=[]
-        input_text = st.text_area("Please Enter Text to Convert to Speech", max_chars=100, key="qw13")
+        input_text = st.text_area("Please Enter Text to Convert to Speech", key="qw13")
         if st.button("Convert to Speech"):
             # Call function to convert input_text to speech
             pass
-            text_upload(a=input_text)
+            # Authenticate replicate client
+            replicate = replicate.Client(api_token='r8_FPd0TDjuTIaLK6pJ86sWou8rWBBD4oC1dzWt0')
+            words=input_text.split()
+            api_input_text = " ".join(words)
+
+            output = replicate.run(
+                "adirik/styletts2:989cb5ea6d2401314eb30685740cb9f6fd1c9001b8940659b406f952837ab5ac",
+                input={
+                    "beta": 0.7,
+                    "seed": 0,
+                    "text": api_input_text,
+                    "alpha": 0.3,
+                    "diffusion_steps": 10,
+                    "embedding_scale": 1.5
+                }
+            )
+            print(output)
 
 
 
@@ -86,10 +85,10 @@ def run():
             if file_up is not None:
                 # Call function to convert file_up to speech
                 pass
-                pdf_upload()
+                print("code for uploading file")
             else:
                 st.warning("Please upload a PDF file.")
-    print(input_text)
+                print(file_up)
 
     
 
